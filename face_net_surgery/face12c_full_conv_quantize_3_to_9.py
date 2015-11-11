e3cfd17f7915e0384f1cee1f7e4f6c2c7aff6610
Anson <anson32330200@gmail.com>
2015-11-11 23:45:31 +0800

    Generalize quantizing to 3~9 bits

--- face_net_surgery/face12c_full_conv_quantize_3_to_9.py
+++ face_net_surgery/face12c_full_conv_quantize_3_to_9.py
@@ -1,8 +1,6 @@
 import numpy as np
 import sys
 
-quantize_bit_num = 3
-
 # ==================  caffe  ======================================
 caffe_root = '/home/anson/caffe-master/'  # this file is expected to be in {caffe_root}/examples
 sys.path.insert(0, caffe_root + 'python')
@@ -48,60 +46,58 @@ params = ['conv1', 'fc2-conv', 'fc3-conv']
 # fc_params = {name: (weights, biases)}
 original_params = {pr: (net.params[pr][0].data, net.params[pr][1].data) for pr in params}
 
-# Load the file to save quantized parameters.
-MODEL_FILE = '/home/anson/caffe-master/models/face_12c/face12c_full_conv.prototxt'
-PRETRAINED = '/home/anson/caffe-master/models/face_12c/face12c_full_conv_quantize_3.caffemodel'
-net_quantized = caffe.Net(MODEL_FILE, PRETRAINED, caffe.TEST)
-params_quantized = ['conv1', 'fc2-conv', 'fc3-conv']
-# conv_params = {name: (weights, biases)}
-quantized_params = {pr: (net_quantized.params[pr][0].data, net_quantized.params[pr][1].data) for pr in params_quantized}
+for quantize_bit_num in range(3, 10):
+    # ==================  load file to save quantized parameters  =======================
+    MODEL_FILE = '/home/anson/caffe-master/models/face_12c/face12c_full_conv.prototxt'
+    PRETRAINED = '/home/anson/caffe-master/models/face_12c/face12c_full_conv_quantize_' \
+                 + str(quantize_bit_num) +'.caffemodel'
+    quantized_model = open(PRETRAINED, 'w')
+    net_quantized = caffe.Net(MODEL_FILE, PRETRAINED, caffe.TEST)
+    params_quantized = params
+    # conv_params = {name: (weights, biases)}
+    quantized_params = {pr: (net_quantized.params[pr][0].data, net_quantized.params[pr][1].data) for pr in params_quantized}
 
-print "\n============face12c_full_conv================="
+    print "\n============face12c_full_conv================="
 
-# transplant
-for pr, pr_quantized in zip(params, params_quantized):
-    quantized_params[pr_quantized][0].flat = original_params[pr][0].flat  # flat unrolls the arrays
-    quantized_params[pr_quantized][1][...] = original_params[pr][1]
+    # transplant
+    for pr, pr_quantized in zip(params, params_quantized):
+        quantized_params[pr_quantized][0].flat = original_params[pr][0].flat  # flat unrolls the arrays
+        quantized_params[pr_quantized][1][...] = original_params[pr][1]
 
-for k, v in net_quantized.params.items():
-    print (k, v[0].data.shape)
-    filters_weights = net_quantized.params[k][0].data
-    filters_bias = net_quantized.params[k][1].data
+    for k, v in net_quantized.params.items():
+        print (k, v[0].data.shape)
+        filters_weights = net_quantized.params[k][0].data
+        filters_bias = net_quantized.params[k][1].data
 
-    if k == 'conv1':
-        a_weight = -1
-        a_bias = -1
-    elif k == 'fc2-conv':
-        a_weight = -3
-        a_bias = 2
-    elif k == 'fc3-conv':
-        a_weight = -4
-        a_bias = 1
+        if k == 'conv1':
+            a_weight = -1
+            a_bias = -1
+        elif k == 'fc2-conv':
+            a_weight = -3
+            a_bias = 2
+        elif k == 'fc3-conv':
+            a_weight = -4
+            a_bias = 1
 
-    b_weight = quantize_bit_num - 1 - a_weight
-    b_bias = quantize_bit_num - 1 - a_bias
-    # print (a_weight, b_weight)
-    # print (a_bias, b_bias)
+        b_weight = quantize_bit_num - 1 - a_weight
+        b_bias = quantize_bit_num - 1 - a_bias
 
-    weightFixedPointList = fixed_point_list(a_weight, b_weight)
-    biasFixedPointList = fixed_point_list(a_bias, b_bias)
-    # print weightFixedPointList
-    # print biasFixedPointList
+        # lists of all possible values under current quantized bit num
+        weightFixedPointList = fixed_point_list(a_weight, b_weight)
+        biasFixedPointList = fixed_point_list(a_bias, b_bias)
 
-    print ("Shape of " + k + " weight params : " + str(filters_weights.shape))
-    print ("Max : " + str(filters_weights.max()) + "  min : " + str(filters_weights.min()))
-    print ("Shape of " + k + " bias params: " + str(filters_bias.shape))
-    print ("Max : " + str(filters_bias.max()) + "  min : " + str(filters_bias.min()))
+        print ("Shape of " + k + " weight params : " + str(filters_weights.shape))
+        print ("Max : " + str(filters_weights.max()) + "  min : " + str(filters_weights.min()))
+        print ("Shape of " + k + " bias params: " + str(filters_bias.shape))
+        print ("Max : " + str(filters_bias.max()) + "  min : " + str(filters_bias.min()))
 
-    for currentNum in np.nditer(filters_weights, op_flags=['readwrite']):
-        currentNum[...] = round_number(currentNum[...], weightFixedPointList)
+        for currentNum in np.nditer(filters_weights, op_flags=['readwrite']):
+            currentNum[...] = round_number(currentNum[...], weightFixedPointList)
 
-    for currentNum in np.nditer(filters_bias, op_flags=['readwrite']):
-        currentNum[...] = round_number(currentNum[...], biasFixedPointList)
+        for currentNum in np.nditer(filters_bias, op_flags=['readwrite']):
+            currentNum[...] = round_number(currentNum[...], biasFixedPointList)
 
-net_quantized.save('/home/anson/caffe-master/models/face_12c/face12c_full_conv_quantize_3.caffemodel')
+    net_quantized.save('/home/anson/caffe-master/models/face_12c/face12c_full_conv_quantize_'
+                       + str(quantize_bit_num) +'.caffemodel')
 
-
-# file_write = open('face_12_full_conv_params_test.txt', 'w')
-# sys.stdout = file_write
-
+    quantized_model.close()
